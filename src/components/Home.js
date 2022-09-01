051bad8534b20105957db4876619477f1b5ee3cc
manaboy-07 <manassehoruebor@gmail.com>
2022-09-01 16:59:27 +0100

    updated changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 /** @format */
 
 import React from "react";
-
+import "../styles/Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className='info pt-5 p-8 '>
+      <h1 className='font-bold text-4xl text-primary text-justify'>
+        Hi there 👋 <p>I'm Manasseh</p>
+      </h1>
+      <small className='text-sm text-secondary'>
+        A Front-end developer but starting backend soon 😁😁😁
+      </small>
+    </div>
+  );
 }
 
 export default Home;
