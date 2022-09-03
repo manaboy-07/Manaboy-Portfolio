c251524eae9c3b75a3e91c318a5d1ba35a63d358
manaboy-07 <manassehoruebor@gmail.com>
2022-09-03 23:49:13 +0100

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,10 @@ function Footer() {
   const date = new Date();
   const year = date.getFullYear();
   return (
-    <div className='text-black bg-accent p-4 w-full'>
+    <div className='text-black bg-slate-200 p-4 w-full'>
       <h3 className='font-bold text-center text-xl'>
-        Copyright <span className='text-extrabold text-2xl'>&copy;</span> | Made
-        with ❤ by Manasseh | {year}
+        Copyright <span className='text-extrabold text-2xl'>©</span> | Made with
+        ❤ by Manasseh | {year}
       </h3>
     </div>
   );
