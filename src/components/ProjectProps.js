83a587b722b499f0366590238ceb2d8926672b48
manaboy-07 <manassehoruebor@gmail.com>
2022-11-01 22:51:57 +0100

    updated jare

--- src/components/ProjectProps.js
+++ src/components/ProjectProps.js
@@ -1,21 +1,28 @@
 /** @format */
 
 import React from "react";
+
 import "../styles/Project.css";
 import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
-function ProjectProps({ description, link }) {
+function ProjectProps({ description, link, src }) {
   return (
     <div>
       <div className='project-overview'>
-        <h2 className="text-bolder text-2xl">{description}</h2>
-        <div className='link-btn mt-4'>
-          <a
-            href={link}
-            target='_blank'
-            className='bg-primary p-2 rounded-md text-white'>
-            View
-            <BsFillArrowUpRightCircleFill className='inline text-xl m-2 text-white' />
-          </a>
+        <div className='project-flex'>
+          <div className='link-btn mt-8'>
+            <h2 className='text-bolder text-2xl my-4'>{description}</h2>
+            <a
+              href={link}
+              target='_blank'
+              className='bg-primary p-2 rounded-md text-white'>
+              View
+              <BsFillArrowUpRightCircleFill className='inline text-xl m-2 text-white' />
+            </a>
+          </div>
+
+          <div>
+            <img src={src} alt='' className='img-pro' />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,16 +2,45 @@
 
 import React from "react";
 import ProjectProps from "./ProjectProps";
-
+import todo from "../images/todo.JPG";
+import meme from "../images/meme.JPG";
+import aos from "../images/aos.JPG";
+import calc from "../images/calc.JPG";
+import filter from "../images/filter.JPG";
+import greek from "../images/greek.JPG";
 function Projects() {
   return (
     <div className='projects'>
-      <ProjectProps description='Meme-Generator ' link='https://meme-gen-gamma.vercel.app/' />
-      <ProjectProps description='Tip Calculator' link='https://tip-calculator-silk.vercel.app/' />
-      <ProjectProps description='Filter Api Project' link='https://filter-api.vercel.app/' />
-      <ProjectProps description='Greek Mythology Quiz' link='https://greek-mythology-quiz-app.vercel.app/' />
-      <ProjectProps description='AOS website ' link='https://website-using-aos-lib.vercel.app/' />
-      <ProjectProps description='Todo List' link='https://todo-list-manaboy-07.vercel.app/' />
+      <ProjectProps
+        description='Meme-Generator '
+        link='https://meme-gen-gamma.vercel.app/'
+        src={meme}
+      />
+      <ProjectProps
+        description='Tip Calculator'
+        src={calc}
+        link='https://tip-calculator-silk.vercel.app/'
+      />
+      <ProjectProps
+        description='Filter Api Project'
+        src={filter}
+        link='https://filter-api.vercel.app/'
+      />
+      <ProjectProps
+        description='Greek Mythology Quiz'
+        src={greek}
+        link='https://greek-mythology-quiz-app.vercel.app/'
+      />
+      <ProjectProps
+        description='AOS website '
+        src={aos}
+        link='https://website-using-aos-lib.vercel.app/'
+      />
+      <ProjectProps
+        description='Todo List'
+        link='https://todo-list-manaboy-07.vercel.app/'
+        src={todo}
+      />
     </div>
   );
 }
